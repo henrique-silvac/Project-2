a22b6502be1a9abcc5f835acedf87371c6913827
Henrique Silva de Carvalho <henrique.silvac@outlook.com>
2021-07-08 12:33:13 -0300

    creating an IBAction

--- Project 2/ViewController.swift
+++ Project 2/ViewController.swift
@@ -29,10 +29,10 @@ class ViewController: UIViewController {
         button2.layer.borderColor = UIColor.lightGray.cgColor
         button3.layer.borderColor = UIColor.lightGray.cgColor
         
-        askQuestion()
+        askQuestion(action: nil)
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
         title = countries[correctAnswer].uppercased()
         
         countries.shuffle()
@@ -42,6 +42,24 @@ class ViewController: UIViewController {
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
         
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else  {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true, completion: nil)
     }
 
 }
